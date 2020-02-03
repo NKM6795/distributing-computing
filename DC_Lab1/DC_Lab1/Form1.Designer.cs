34918aad0ddb7ffc0f664fdef7beef84fd8a86df
NKM6795 <bodyaufml@gmail.com>
2020-02-03 15:34:58 +0200

    Added Lab1a

--- DC_Lab1/DC_Lab1/Form1.Designer.cs
+++ DC_Lab1/DC_Lab1/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace DC_Lab1
+﻿using System;
+using System.Windows.Forms;
+
+namespace DC_Lab1
 {
 	partial class Form1
 	{
@@ -28,13 +31,74 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Items.AddRange(new object[] {
+			"4",
+			"3",
+			"2",
+			"1",
+			"0"});
+			this.listBox1.Location = new System.Drawing.Point(43, 46);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(76, 134);
+			this.listBox1.TabIndex = 0;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.Items.AddRange(new object[] {
+            "4",
+            "3",
+            "2",
+            "1",
+			"0"});
+			this.listBox2.Location = new System.Drawing.Point(632, 46);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(76, 134);
+			this.listBox2.TabIndex = 1;
+			this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(188, 46);
+			this.trackBar1.Maximum = 100;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(402, 45);
+			this.trackBar1.TabIndex = 3;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.listBox2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.trackBar1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
+
 		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.TrackBar trackBar1;
 	}
 }
 
--- DC_Lab1/DC_Lab1/Form1.cs
+++ DC_Lab1/DC_Lab1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,122 @@ namespace DC_Lab1
 {
 	public partial class Form1 : Form
 	{
+		private delegate void UpdateTrackBarCallDelegate(bool is_increment);
+		private delegate void CloseFormCallDelegate();
+		private Thread thread1 = null;
+		private Thread thread2 = null;
+		private volatile bool is_running = true;
+		private static int iteration_count = 10000000;
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.trackBar1.Value = 50;
+
+			this.thread1 = new Thread(() =>
+			{
+				while (this.is_running)
+				{
+					UpdateTrackBar(false);
+
+					int i = 0;
+					while (i < iteration_count)
+					{
+						++i;
+					}
+					//Thread.Sleep(10);
+				}
+			}
+			);
+
+			this.thread2 = new Thread(() =>
+			{
+				while (this.is_running)
+				{
+					UpdateTrackBar(true);
+
+					int i = 0;
+					while (i < iteration_count)
+					{
+						++i;
+					}
+					//Thread.Sleep(10);
+				}
+			}
+			);
+
+			this.thread1.Start();
+			this.thread2.Start();
+
+
+			this.listBox1.SelectedIndex = 2;
+			this.listBox2.SelectedIndex = 2;
+		}
+		private void Form1_Closing(object sender, FormClosingEventArgs e)
+		{
+			if (this.is_running)
+			{
+				this.is_running = false;
+				e.Cancel = true;
+
+				new Thread(() =>
+				{
+					Thread.Sleep(50);
+					CloseForm();
+				}).Start();
+			}
+		}
+
+
+		private void UpdateTrackBar(bool is_increment)
+		{
+			if (this.trackBar1.InvokeRequired)
+			{
+				UpdateTrackBarCallDelegate d = new UpdateTrackBarCallDelegate(UpdateTrackBar);
+				this.trackBar1.Invoke(d, new object[] { is_increment });
+			}
+			else
+			{
+				int val = this.trackBar1.Value;
+
+				if (!is_increment && val > 10)
+				{
+					--this.trackBar1.Value;
+				}
+				else if (is_increment && val < 90)
+				{
+					++this.trackBar1.Value;
+				}
+			}
+		}
+
+		
+
+		private void CloseForm()
+		{
+			if (this.InvokeRequired)
+			{
+				CloseFormCallDelegate d = new CloseFormCallDelegate(CloseForm);
+				this.trackBar1.Invoke(d, new object[] {});
+			}
+			else
+			{
+				this.Close();
+			}
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			this.thread1.Priority = (ThreadPriority) Enum.Parse(typeof(ThreadPriority), this.listBox1.SelectedItem.ToString());
+		}
+
+		private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			this.thread2.Priority = (ThreadPriority) Enum.Parse(typeof(ThreadPriority), this.listBox2.SelectedItem.ToString());
 		}
 	}
 }
