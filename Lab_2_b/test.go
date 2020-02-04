340c89827c1690747acb31b4ba6d084aaa10423f
Nirfiolo <bodyaufml@gmail.com>
2020-02-04 21:15:38 +0200

    Hot-fix in Lab_2_b

--- Lab_2_b/test.go
+++ Lab_2_b/test.go
@@ -7,8 +7,8 @@ import (
 var task_list = []string{ "task_0", "task_1", "task_2", "task_3", "task_4", "task_5", "task_6", "task_7", "task_8", "task_9" }
 var size = len(task_list)
 
-var take_away = make(chan string)
-var load = make(chan string)
+var take_away = make(chan string, size)
+var load = make(chan string, size)
 
 var done = make(chan bool)
 
