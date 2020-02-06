1704489fc931c8ddc11cf15712dde05790966996
NKM6795 <bodyaufml@gmail.com>
2020-02-06 18:01:42 +0200

    Added Lab_6_a

---
+++ Lab_6_ab/Lab_6_a/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Lab_6_a
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonNext = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// buttonNext
+			// 
+			this.buttonNext.Location = new System.Drawing.Point(797, 136);
+			this.buttonNext.Name = "buttonNext";
+			this.buttonNext.Size = new System.Drawing.Size(186, 64);
+			this.buttonNext.TabIndex = 0;
+			this.buttonNext.Text = "Next";
+			this.buttonNext.UseVisualStyleBackColor = true;
+			this.buttonNext.Click += new System.EventHandler(this.ButtonNext_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1029, 861);
+			this.Controls.Add(this.buttonNext);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonNext;
+	}
+}
+
